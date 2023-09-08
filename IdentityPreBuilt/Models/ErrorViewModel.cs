c9c1f0875702f3a0f410195bac163b83b9e087ff
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-09-08 11:46:16 +0300

    DICLAIMER!! This commit contains redundant projects that I used to create new functionalities from existing projects. I DO NOT RECOMMEND USING THIS COMMIT

---
+++ IdentityPreBuilt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityPreBuilt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
